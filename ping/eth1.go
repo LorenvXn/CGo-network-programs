17c36a0be2c8237e374d187c1f3503860b85b32d
Satanette <predark.dante@gmail.com>
2017-01-18 16:18:44 +0200

    some CGo stuff and network

---
+++ ping/eth1.go
@@ -0,0 +1,34 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+ void ping()
+{
+        system("ping -c 2 localhost");
+}
+
+*/
+import "C"
+
+func main() {
+
+    fd, err := syscall.Socket(syscall.AF_PACKET, syscall.SOCK_RAW, syscall.ETH_P_ALL)
+    if err != nil {
+        fmt.Println("Error: " + err.Error())
+        return
+    }
+    fmt.Println("Obtained fd ", fd)
+    defer syscall.Close(fd)
+
+    interf, err := net.InterfaceByName("eth0")
+    if err != nil {
+        fmt.Println("Could not find eth0 interface")
+        return
+    }
+         fmt.Println("Interface hw address: ", interf.HardwareAddr)
+         fmt.Println("MTU hw interface: ", interf.MTU)
+         fmt.Println("Pinging...")
+         C.ping()
+
+
+}
+
